5e13e4174f79b05ea41f4777bc28434f6cb29eac
iamgillespie <gailen.gillespie@gmail.com>
2022-06-18 14:51:12 -0500

    static root setting

--- diasotpublishing/settings.py
+++ diasotpublishing/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = '/home/diasot/diasot/groot/static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
